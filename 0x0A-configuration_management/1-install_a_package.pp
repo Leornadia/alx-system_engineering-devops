4bca89ecc85ba6cedbd4829ecaf4b009a5777b45
Leornadia <leornadiajose@gmail.com>
2024-04-19 08:03:46 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# This Puppet manifest installs Flask version 2.1.0 using Pip3
-
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+class install_flask {
+  package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip',
+  }
 }
+
